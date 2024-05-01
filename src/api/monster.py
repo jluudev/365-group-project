068e0746789d58ef537c82ef0c8dc4d96f141150
Jessica Luu <jessica.luu0002@gmail.com>
2024-05-01 13:30:28 -0700

    v1: add routers

---
+++ src/api/monster.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter, Depends
+from enum import Enum
+from pydantic import BaseModel
+from src.api import auth
+
+router = APIRouter(
+    prefix="/monster",
+    tags=["monster"],
+    dependencies=[Depends(auth.get_api_key)],
+)
